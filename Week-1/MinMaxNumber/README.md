a7283d83f0bfd65d2962c681f395f9583f9a3c2e
AtacanErdogan <atacanerdogan2@gmail.com>
2023-09-14 10:38:25 +0300

    add 'MinMaxNumber' project

---
+++ Week-1/MinMaxNumber/README.md
@@ -0,0 +1,17 @@
+A program that calculates the minimum and maximum of the entered numbers by taking data from the user.
+
+Case
+
+How many numbers will you enter:4
+
+Please enter the 1. number :16
+
+Please enter the 2. number :-22
+
+Please enter the 3. number :-15
+
+Please enter the 4. number :100
+
+Min Number : -22
+
+Max Number : 100
---
+++ Week-1/MinMaxNumber/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Variable identification
+        int n, number;
+        int minNumber = 0;
+        int maxNumber = 0;
+
+
+        // Retrieving data from the user
+        System.out.print("How many numbers will you enter:");
+        n = input.nextInt();
+
+        // Calculation Section
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Please enter the " + i + ". number :");
+            number = input.nextInt();
+
+            if (i == 1) {
+                minNumber = number;
+                maxNumber = number;
+            } else {
+                if (number < minNumber) {
+                    minNumber = number;
+                }
+                if (number > maxNumber) {
+                    maxNumber = number;
+                }
+            }
+
+        }
+
+        System.out.print("Min Number : " + minNumber + "\nMax Number : " + maxNumber);
+
+    }
+}
