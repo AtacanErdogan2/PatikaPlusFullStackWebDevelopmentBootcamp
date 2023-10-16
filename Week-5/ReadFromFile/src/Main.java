b25123efb94bbf448073f654039e7b0021b1d004
AtacanErdogan <atacanerdogan2@gmail.com>
2023-10-16 17:31:06 +0300

    clean code changes

--- Week-5/ReadFromFile/src/Main.java
+++ Week-5/ReadFromFile/src/Main.java
@@ -1,45 +1,38 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
-            String numbers = "5\n10\n20\n12\n33";
-            //    String[] numbers = {"5", "10", "20", "12", "33"};
+
+            String[] numbers = {"5", "10", "20", "12", "33"};
             File newFile = new File("numbers.txt");
             FileWriter input = new FileWriter(newFile);
             BufferedWriter output = new BufferedWriter(input);
 
-//            for (String str : numbers) {
-//                Arrays.stream(numbers).skip(numbers.length -1);
-//                output.write(str + "\n");
-//
-//            }
-
-            output.write(numbers);
+            for (String str : numbers) {
+                output.write(str + "\n");
+            }
 
             output.close();
 
             FileReader reader = new FileReader(newFile);
             BufferedReader bufferedReader = new BufferedReader(reader);
 
-            int n = Integer.parseInt(bufferedReader.readLine());
-
+            String line;
             int sum = 0;
 
-            while (n != -1) {
+            while ((line = bufferedReader.readLine()) != null) {
+                int n = Integer.parseInt(line);
                 sum += n;
-                System.out.println(sum);
-                n = Integer.parseInt(bufferedReader.readLine());
             }
 
-
+            System.out.println(sum);
             bufferedReader.close();
 
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+
     }
 }
