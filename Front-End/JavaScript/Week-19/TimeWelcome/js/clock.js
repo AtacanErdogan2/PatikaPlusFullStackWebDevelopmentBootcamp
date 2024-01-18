118f19c0f49a1d464a8c7deb1aa9e3d375e55eda
AtacanErdogan <atacanerdogan2@gmail.com>
2024-01-18 16:25:02 +0300

    update

--- Front-End/JavaScript/Week-19/TimeWelcome/js/clock.js
+++ Front-End/JavaScript/Week-19/TimeWelcome/js/clock.js
@@ -5,22 +5,28 @@ let nameDOM = document.querySelector("#myName");
 nameDOM.innerHTML = namePrompt;
 
 function showTime() {
-    const date = new Date();
-    let d = getDayName();
-    let h = date.getHours();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s + " " + d;
-    setTimeout(showTime, 500);
-  }
-  
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
+  const date = new Date();
+  let d = getDayName();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("myClock").innerHTML =
+    h + ":" + m + ":" + s + " " + d;
+  setTimeout(showTime, 500);
+}
 
-  function getDayName(date = new Date(), locale = 'en-US') {
-    return date.toLocaleDateString(locale, {weekday: 'long'});
-  }
+// let div = document.querySelector('#myClock');
+// div.addEventListener('load', showTime());
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+function getDayName(date = new Date(), locale = "en-US") {
+  return date.toLocaleDateString(locale, { weekday: "long" });
+}
